2a9d3abac0fa37bb352246add84c888d619bee2e
Kevin Bian <kaiwenbian107@gmail.com>
2024-04-14 13:35:31 -0700

    add

--- torch_run.py
+++ torch_run.py
@@ -62,8 +62,8 @@ def main(cfg: "DictConfig"):  # noqa: F821
         total_frames=cfg.collector.total_frames,
         device=device,
         storing_device=device,
-        #split_trajs=False,
-        max_frames_per_traj=-1,
+        split_trajs=False,
+        #max_frames_per_traj=-1,
     )
 
 
